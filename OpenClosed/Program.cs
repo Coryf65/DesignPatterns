764b91cef1be7d181ba5669976be899df1d9d8f2
Cory Fabian <Coryf65@users.noreply.github.com>
2022-12-11 17:17:28 -0600

    Showing example of Open Closed
    Demo showing adding a filter and say we needed to add more filters and combinations of them.

---
+++ OpenClosed/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Open Closed");
+Console.WriteLine("___________");
+
+var apple = new Product("Apple", Color.Red, Size.Small);
+var spaling = new Product("Banana Sapling", Color.Green, Size.Medium);
+var tShirt = new Product("T-Shirt", Color.Blue, Size.Small);
+
+Product[] products = {apple, spaling, tShirt};
+
+var filter = new ProductFilter();
+Console.WriteLine("Green products (old)");
+foreach (var product in filter.FilterByColor(products, Color.Green))
+{
+	Console.WriteLine($"- {product.Name} is green");
+}
+
+
+public class Product
+{
+	public string Name;
+	public Color Color;
+	public Size Size;
+
+	public Product(string name, Color color, Size size)
+	{
+		Name = name;
+		Color = color;
+		Size = size;
+	}
+}
+
+// filtering a collection
+public class ProductFilter
+{
+	public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+	{
+		foreach (var item in products)
+		{
+			if (item.Size == size)
+			{
+				// found product with the same size
+				yield return item;
+			}
+		}
+	}
+
+	public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+	{
+		foreach (var item in products)
+		{
+			if (item.Color == color)
+			{
+				// found product with the same color
+				yield return item;
+			}
+		}
+	}
+}
+
+public enum Color
+{
+	Red,
+	Green,
+	Blue,
+	Yellow
+}
+
+public enum Size
+{
+	Small,
+	Medium,
+	Large
+}
