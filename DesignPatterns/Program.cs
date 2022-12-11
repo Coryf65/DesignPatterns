e0077643d189bbdce4b99a9bd4409aaf1dc8fb29
Cory Fabian <Coryf65@users.noreply.github.com>
2022-12-11 16:36:42 -0600

    Single Responsibility Demo
    new demo to show an example of S. in SOLID

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,2 +1,81 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using System.IO;
+
+Console.WriteLine("Single Responsibility Demo");
+Console.WriteLine("__________________________\n");
+
+var j = new Journal();
+
+j.AddEntry("I ate a doughnut today.");
+j.AddEntry("Drank one soda.");
+
+Console.WriteLine(j);
+
+var p = new Persistence();
+var filename = @"c:\temp\journal.txt";
+p.SaveToFile(j, filename, true);
+
+Process process = new();
+process.StartInfo.FileName = "notepad.exe";
+process.StartInfo.Arguments = filename;
+
+// opens our journal in notepad!
+process.Start();
+
+
+
+public class Journal
+{
+	private readonly List<string> entries = new();
+	private static int count = 0;
+
+	public int AddEntry(string text)
+	{
+		entries.Add($"{count++}: {text}");
+		return count; // example of the memento pattern
+	}
+
+	public void RemoveEntry(int index)
+	{
+		entries.RemoveAt(index);
+	}
+
+	public override string ToString()
+	{
+		return string.Join(Environment.NewLine, entries);
+	}
+
+
+	// So all of the saving and loading is too much for one class
+	// We should take the data persistence part out and make a new class
+	//public void Save(string filename)
+	//{
+	//	File.WriteAllText(filename, ToString());
+	//}
+
+	//public string Load(string filename)
+	//{
+	//	return File.ReadAllText(filename);
+	//}
+}
+
+/// <summary>
+/// Now we have a seperation of concerns HERE we are saving and loading journal files
+/// And the Journal class is focused on keeping enteries and maintaining the Journal
+/// </summary>
+public class Persistence
+{
+	public void SaveToFile(Journal j, string filename, bool overwrite = false)
+	{
+		if (overwrite || !File.Exists(filename))
+		{
+			File.WriteAllText(filename, j.ToString());
+		}
+	}
+
+	public string Load(string filename)
+	{
+		return File.ReadAllText(filename);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # DesignPatterns
+
+Notes for different design patterns in programming. This aplies to almost any language but I am focusing on C# and .NET.
+
+## SOLID Design Principles
+
+- Introduced by Robert C. Martin (aka: Uncle Bob)
+
+> See Projects with related names for a code example. I am keeping all code in one file on purpose. To keep projects in groups and keep things simple for demonstration.
+
+1. Single Responsibility
+
+	- A single class should have one reason to change
+
+2. Open-Closed Principle
+
+3. Liskov Substitution Principle
+
+4. Interface Segregation Principle
+
+5. Dependency Inversion Principle
+
+### Creational
+
+	- Builder
+
+	- Factories
+
+		a. Abstract Factory
+
+		b. Factory Method
+
+	- Prototype
+
+	- Singleton
+
+### Structual
+
+	- Adapter
+
+	- Bridge
+
+	- Composite
+
+	- Decorator
+
+	- Facade
+
+	- Flyweight
+
+	- Proxy
+
+### Behavioral
+
+	- Chain of Responsibility
+
+	- Command
+
+	- Interpreter
+
+	- Iterator
+
+	- Mediator
+
+	- Memento
+
+	- Null Object
+
+	- Observer
+
+	- State
+
+	- Strategy
+
+	- Template Method
+
+	- Visitor
